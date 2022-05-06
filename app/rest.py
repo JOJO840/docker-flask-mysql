7167e178f5afa0cd52db4fdb6d95f0fcaceb84f6
JOJO840 <josip840@gmail.com>
2022-05-06 16:17:23 +0200

    making changes not complete3

--- app/rest.py
+++ app/rest.py
@@ -19,7 +19,19 @@ def persons():
     return resp
 
 @app.route('/persons/POST', methods=['POST'])
-        
+def persons_post():
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    request.method == 'POST'
+
+    cursor.execute("INSERT INTO persons (id, name, email, phone, adress) values (3, Petter, Earth2 ) ")
+    conn.commit()
+
+    rows = cursor.fetchall()        
+    resp = jsonify(rows)
+    resp.status_code = 200
+
+    return resp
 
 
 if __name__ == "__main__":
--- db/persons.sql
+++ db/persons.sql
@@ -4,12 +4,10 @@ use Persons;
 CREATE TABLE `persons` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` int unsigned NOT NULL,
   `address` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert into `persons`(`id`,`name`,`email`,`phone`,`address`) values
-(1,'Josip Jovanovic','sroy@gmail.com',2147483647,'Earth'),
-(2,'Rahul Kumar','rahul@gmail.com',34256780,'Mars');
+insert into `persons`(`id`,`name`,`address`) values
+(1,'Josip Jovanovic','Earth'),
+(2,'Rahul Kumar','Mars');
