7ba57b40db138c8a5962bb49b36096a38df68a08
JOJO840 <josip840@gmail.com>
2022-05-07 10:25:44 +0200

    edit-removed insert from sql file

--- db/persons.sql
+++ db/persons.sql
@@ -6,8 +6,5 @@ CREATE TABLE `persons` (
   `name` varchar(50) NOT NULL,
   `address` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert into `persons`(`name`,`address`) values
-('Josip Jovanovic','Earth'),
-('Rahul Kumar','Mars');
